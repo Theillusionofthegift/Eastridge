c90461c75abe7f58a163e3c44bc4f74ec98e4acd
Dylan <theillusionofthegift@gmail.com>
2021-01-25 15:49:15 -0800

    Added a title bar component and added it my Illusion page

---
+++ client/src/component/TitleBox.js
@@ -0,0 +1,11 @@
+import './TitleBox.css';
+
+function TitleBox() {
+  return (
+    <div className="titleBox">
+        <h1 className="title">Illusion!</h1>
+    </div>
+  );
+}
+
+export default TitleBox;
