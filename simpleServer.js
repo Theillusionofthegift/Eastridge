3f3e025ac91be00e09339f4ad72aa7cbd19141cc
Dylan Eastridge <49111933+Theillusionofthegift@users.noreply.github.com>
2021-01-25 17:13:34 -0800

    Update simpleServer.js

--- simpleServer.js
+++ simpleServer.js
@@ -2,46 +2,39 @@ const http = require('http');
 const fs =  require('fs');
 
 const requestListener = function (req, res) {
-    if(req.url === '/users' && req.method === 'GET')
-    {
+    if(req.url === '/users' && req.method === 'GET') {
         //read the users.html file returning a list of users
         fs.readFile('users.html', function(err, data) {
             if(err) { throw err;}
-            
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(data);
             return res.end();
           });
     }
-    else if (req.url === '/' && req.method === 'POST')
-    {
+    else if(req.url === '/' && req.method === 'POST') {
         //read data into buffer from request object
         let buffer = '';
-        req.on('data', chunk => {
-             buffer += chunk;
-          });
+        req.on('data', chunk => { buffer += chunk; });        
         //return html with JSON content in the main 
         req.on('end', () => { 
             //parse buffer to JSON object
-           jsonContent = JSON.parse(buffer) 
-           res.writeHead(200, { 'Content-Type': 'text/html'});
+            jsonContent = JSON.parse(buffer) 
+            res.writeHead(200, { 'Content-Type': 'text/html'});
             res.end(`
                 <!doctype html>
-                <html>
-                <body>
-                    <main>
-                        ${JSON.stringify(jsonContent)}
-                    </main>
-                </body>
-                </html>`);
+                    <html>
+                        <body>
+                            <main>
+                                ${JSON.stringify(jsonContent)}
+                            </main>
+                        </body>
+                    </html>`);
           })
     }
-    else
-    {
+    else {
        res.end('This is Home Page');
     }
     
-   
 }
 const server = http.createServer(requestListener);
 server.listen(8080);
